e63fe0196e7f1a0c285ffed560d0eae9a815f163
sangcomz <dev.seokwon2@gmail.com>
2025-10-28 00:45:25 +0900

    feat: simplify Next.js configuration by removing custom webpack externals and adding turbopack support

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@libsql/client', '@prisma/adapter-libsql'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...config.externals, '_http_common'];
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
